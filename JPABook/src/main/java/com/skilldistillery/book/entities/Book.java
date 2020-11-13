2c36f5668637000bf1caef442bd9bfc3abb3c306
Bronson Spies <spiesbronson@gmail.com>
2020-11-13 15:52:28 -0700

    setup JPA

---
+++ JPABook/src/main/java/com/skilldistillery/book/entities/Book.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.book.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+@Id
+@GeneratedValue(strategy =)
+}
