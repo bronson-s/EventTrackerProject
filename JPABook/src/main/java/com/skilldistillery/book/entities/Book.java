305cdf86b72d5817b2c2f0347fcfb233ea3002e5
Bronson Spies <spiesbronson@gmail.com>
2020-11-13 16:11:10 -0700

    junit testing done

--- JPABook/src/main/java/com/skilldistillery/book/entities/Book.java
+++ JPABook/src/main/java/com/skilldistillery/book/entities/Book.java
@@ -2,10 +2,66 @@ package com.skilldistillery.book.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Book {
 @Id
-@GeneratedValue(strategy =)
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+private String title;
+
+
+public Book(int id, String title) {
+	super();
+	this.id = id;
+	this.title = title;
+}
+public Book() {
+	super();
+}
+
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + id;
+	return result;
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Book other = (Book) obj;
+	if (id != other.id)
+		return false;
+	return true;
+}
+
+
+
+@Override
+public String toString() {
+	return "Book [id=" + id + ", title=" + title + "]";
+}
 }
--- JPABook/src/test/java/com/skilldistillery/book/entities/BookTest.java
+++ JPABook/src/test/java/com/skilldistillery/book/entities/BookTest.java
@@ -1,25 +1,38 @@
 package com.skilldistillery.book.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BookTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Book book;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("BookPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		book = em.find(Book.class, 1);
 	}
 
 	@AfterEach
@@ -27,8 +40,10 @@ class BookTest {
 	}
 
 	@Test
+	@DisplayName("testing book mapping")
 	void test() {
-		fail("Not yet implemented");
+	assertNotNull(book);
+	assertEquals("The Book of Job", book.getTitle());
 	}
 
 }
