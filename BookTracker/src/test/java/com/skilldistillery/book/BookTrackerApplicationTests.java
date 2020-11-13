e5bc17381ef34cd16e26d2d92b4b3e4049e542b7
Bronson Spies <spiesbronson@gmail.com>
2020-11-13 16:42:53 -0700

    boot project setup

---
+++ BookTracker/src/test/java/com/skilldistillery/book/BookTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
