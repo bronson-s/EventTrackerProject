70714539af49a0c0d9a094508a1561716336e067
Bronson Spies <spiesbronson@gmail.com>
2021-01-28 10:03:43 -0700

    re mapped entities and added components

--- ngBook/src/app/app.module.ts
+++ ngBook/src/app/app.module.ts
@@ -8,11 +8,15 @@ import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
+    NavBarComponent,
+    AboutComponent,
 
   ],
   imports: [
